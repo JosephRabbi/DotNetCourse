abbaa07c79fa800dfc1a51bd8d2b2c41d1009377
JosephRabbi <104782514+JosephRabbi@users.noreply.github.com>
2022-05-21 10:13:52 +0530

    Add files via upload
    Welcome Testing

---
+++ First.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Hello
+{
+  class ProgramA
+  {
+    ststic void Main(string [] args)
+    {
+      Console.WriteLine("Enter Your Name: ");
+      string Name = Console.ReadLine();
+      Console.WriteLine("Hello "+Name+" Welcome to Git Hub ");
+    }
+  }
+}
